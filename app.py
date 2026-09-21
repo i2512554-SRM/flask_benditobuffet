de7ce6ccde6ec2bf9c6d949dd180b8bf992b53a9
i2512554-SRM <i2512554@continental.edu.pe>
2026-09-21 16:29:40 -0500

    librerias que me faltaban

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
+from flask_login import login_required
 from dotenv import load_dotenv
 from pathlib import Path
 
